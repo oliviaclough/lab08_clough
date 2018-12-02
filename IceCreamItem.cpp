0b7dab56ba84c9b2ebeac440f8204c47a9b1dec0
oliviaclough <38055262+oliviaclough@users.noreply.github.com>
2018-12-02 15:57:55 -0800

    Create IceCreamItem.cpp

---
+++ IceCreamItem.cpp
@@ -0,0 +1,14 @@
+//Olivia Clough
+//IceCreamItem.cpp
+
+#include "IceCreamItem.h"
+
+#include <string>
+using namespace std;
+
+IceCreamItem::IceCreamItem (std::string size){
+  //constructor
+  size = size;
+}
+
+//others are pure virtual and set to 0 in the body of class definition
