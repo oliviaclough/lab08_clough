174f368ba5d18e2346c5377a71d871fb55988714
oliviaclough <38055262+oliviaclough@users.noreply.github.com>
2018-12-05 11:15:15 -0800

    Update PreMadeItem.h

--- PreMadeItem.h
+++ PreMadeItem.h
@@ -5,19 +5,21 @@
 #define PREMADEITEM_H
 
 #include "IceCreamItem.h"
+//again include the parent header file 
+
 #include <string>
 
 class PreMadeItem : public IceCreamItem
 {
  public:
   //functions
-  virtual std::string composeItem();
+  virtual std::string composeItem(); //how to print out the item
 
-  virtual double getPrice();
+  virtual double getPrice(); //returns the price
 
-  PreMadeItem(std::string name, std::string size);
+  PreMadeItem(std::string name, std::string size); //constructor - given name and size but also need to set price within this
 
-  virtual ~PreMadeItem();
+  virtual ~PreMadeItem(); //don't need ???
   
  private:
   //vars
