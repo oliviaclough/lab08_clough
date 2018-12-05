6539c649e4dce55c7716546e0935c4769307d946
oliviaclough <38055262+oliviaclough@users.noreply.github.com>
2018-12-05 11:18:20 -0800

    Update PreMadeItem.cpp

--- PreMadeItem.cpp
+++ PreMadeItem.cpp
@@ -1,6 +1,8 @@
 //Olivia Clough
 //PreMadeItem.cpp
 
+
+//include both inherited and parent
 #include "PreMadeItem.h"
 #include "IceCreamItem.h"
 
@@ -8,6 +10,8 @@ using namespace std;
 #include <string>
 
 PreMadeItem::PreMadeItem (std::string name, std::string size) : IceCreamItem(size)  {
+    //given size, set the price
+    
     if (size == "small")
         { price = 4.00; }
     else if (size == "medium")
@@ -15,11 +19,14 @@ PreMadeItem::PreMadeItem (std::string name, std::string size) : IceCreamItem(siz
     else if (size == "large")
         {price = 7.50; }
     
+    //need to set name and size to use in print out
     this->name = name; 
     this->size = size;
 }
 
 std::string PreMadeItem::composeItem() {
+    //create the string using the given parameters 
+    
   std::string pre = "";
   std::string pri = std::to_string(price);
   //std::string si = size;
@@ -35,4 +42,6 @@ double PreMadeItem::getPrice() {
 
 PreMadeItem::~PreMadeItem() {
   //STUB -- not needed
+    
+    //can't use delete on things that are not created on the heap
 }
